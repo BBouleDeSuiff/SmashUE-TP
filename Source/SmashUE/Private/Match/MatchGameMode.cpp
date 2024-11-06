bf177a0411d95144214c75cc2d9b292b4949293f
BBouleDeSuiff <emaouardi@gmail.com>
2024-11-06 10:37:10 +0100

    [ADD] Spawn Characters

--- Source/SmashUE/Private/Match/MatchGameMode.cpp
+++ Source/SmashUE/Private/Match/MatchGameMode.cpp
@@ -1,5 +1,65 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Match/MatchGameMode.h"
+#include "Characters/SmashCharacter.h"
+#include "Arena/ArenaPlayerStart.h"
+#include "Kismet/GameplayStatics.h"
 
+void AMatchGameMode::FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& ResultsActors)
+{
+    TArray<AActor*> FoundActors;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), AArenaPlayerStart::StaticClass(), FoundActors);
+
+    for (int i = 0; i < FoundActors.Num(); ++i)
+    {
+        AArenaPlayerStart* ArenaPlayerStartActor = Cast<AArenaPlayerStart>(FoundActors[i]);
+        if (ArenaPlayerStartActor == nullptr) continue;
+
+        ResultsActors.Add(ArenaPlayerStartActor);
+    }
+}
+
+void AMatchGameMode::SpawnCharacters(const TArray<AArenaPlayerStart*>& SpawnPoints)
+{
+    for (AArenaPlayerStart* SpawnPoint : SpawnPoints)
+    {
+        EAutoReceiveInput::Type InputType = SpawnPoint->AutoReceiveInput.GetValue();
+        TSubclassOf<ASmashCharacter> SmashCharacterClass = GetSmashCharacterClassFromInputType(InputType);
+        if (SmashCharacterClass == nullptr) continue;
+
+        ASmashCharacter* NewCharacter = GetWorld()->SpawnActorDeferred<ASmashCharacter>(
+            SmashCharacterClass,
+            SpawnPoint->GetTransform()
+        );
+
+        if (NewCharacter == nullptr) continue;
+        NewCharacter->AutoPossessPlayer = SpawnPoint->AutoReceiveInput;
+        NewCharacter->FinishSpawning(SpawnPoint->GetTransform());
+
+        CharactersInsideArena.Add(NewCharacter);
+    }
+}
+
+void AMatchGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    TArray<AArenaPlayerStart*> PlayerStartsPoints;
+    FindPlayerStartActorsInArena(PlayerStartsPoints);
+    SpawnCharacters(PlayerStartsPoints);
+}
+
+TSubclassOf<ASmashCharacter> AMatchGameMode::GetSmashCharacterClassFromInputType(EAutoReceiveInput::Type InputType) const
+{
+    switch (InputType)
+    {
+    case EAutoReceiveInput::Player0:
+        return SmashCharacterClassP0;
+    case EAutoReceiveInput::Player1:
+        return SmashCharacterClassP1;
+    case EAutoReceiveInput::Player2:
+        return SmashCharacterClassP2;
+    case EAutoReceiveInput::Player3:
+        return SmashCharacterClassP3;
+    default:
+        return nullptr;
+    }
+}
--- Source/SmashUE/Public/Match/MatchGameMode.h
+++ Source/SmashUE/Public/Match/MatchGameMode.h
@@ -1,17 +1,42 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "MatchGameMode.generated.h"
 
+class ASmashCharacter;
+class AArenaPlayerStart;
+
 /**
- * 
+ *
  */
 UCLASS()
 class SMASHUE_API AMatchGameMode : public AGameModeBase
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    virtual void BeginPlay() override;
+
+protected:
+    UPROPERTY()
+    TArray<ASmashCharacter*> CharactersInsideArena;
+
+    UPROPERTY(EditAnywhere)
+    TSubclassOf<ASmashCharacter> SmashCharacterClassP0;
+
+    UPROPERTY(EditAnywhere)
+    TSubclassOf<ASmashCharacter> SmashCharacterClassP1;
+
+    UPROPERTY(EditAnywhere)
+    TSubclassOf<ASmashCharacter> SmashCharacterClassP2;
+
+    UPROPERTY(EditAnywhere)
+    TSubclassOf<ASmashCharacter> SmashCharacterClassP3;
+
+private:
+    void FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& ResultsActors);
+    void SpawnCharacters(const TArray<AArenaPlayerStart*>& SpawnPoints);
+    TSubclassOf<ASmashCharacter> GetSmashCharacterClassFromInputType(EAutoReceiveInput::Type InputType) const;
+
 };
